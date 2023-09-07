ec7b73f3ee5fa056ccce0cafaffbc233cf139528
deepak.pawar <deepak.pawar@blazeclan.com>
2023-09-07 12:34:41 +0530

    prod

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-export Lambda_function_name =  "lambda_2"
-export CFT_name = "ambda-2-lambdafuntion"
-export description = "Demo_update_prod"
+export Lambda_function_name="lambda_3"
+export CFT_name="lambda-3-lambdafuntion"
+export description="Demo_update_uat"
