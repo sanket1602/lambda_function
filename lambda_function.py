c2420123248e628f58c8dd642d3239922d9a27d2
deepak.pawar <deepak.pawar@blazeclan.com>
2023-09-07 12:05:29 +0530

    prod

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
 Test deployment
 
 +++++++++++++++++++++++++++++++
-env test
+env prod
---
+++ run.sh
@@ -0,0 +1,3 @@
+export Lambda_function_name =  "lambda_3"
+export CFT_name = "ambda-3-lambdafuntion"
+export description = "Demo_update_prod"
