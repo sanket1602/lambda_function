0dc7f1e5041a60504dce228b45c84b322e95a85c
deepak.pawar <deepak.pawar@blazeclan.com>
2023-09-25 16:36:36 +0530

    test

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-export Lambda_function_name="lambda_3"
-export CFT_name="lambda-3-lambdafuntion"
-export description="Demo_update_uat"
+#!/bin/bash
+
+# Read lambda name and lambda cftname from your shell file
+# source run.sh
+
+# Setthe environment variables
+export LAMBDA_NAME="lambda_3"
+export LAMBDA_CFT_NAME="lambda-3-lambdafuntion"
+export LAMBDA_YML_NAME="lambda_3.yml"
