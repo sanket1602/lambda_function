3987f3ed2e6e2523971d41fd66da48e9d118a2db
deepak.pawar <deepak.pawar@blazeclan.com>
2023-09-25 17:00:38 +0530

    test

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Read lambda name and lambda cftname from your shell file
-source run.sh
-
 # Setthe environment variables
 export LAMBDA_NAME="lambda_3"
 export LAMBDA_CFT_NAME="lambda-3-lambdafuntion"
