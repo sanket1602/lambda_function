a051cd17d51bffe6a7beb4eb7aff4ea95fe2f53c
deepak.pawar <deepak.pawar@blazeclan.com>
2023-10-16 16:10:37 +0530

    test

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Setthe environment variables
-export LAMBDA_NAME="lambda_3"
-export LAMBDA_CFT_NAME="lambda-3-lambdafuntion"
+LAMBDA_NAME="lambda_3"
+#LAMBDA_CFT_NAME="lambda-3-lambdafuntion"
 # export LAMBDA_YML_NAME="lambda_3.yml"
 # export LAMBDA_ZIP_NAME="lambda_3.zip"
+
+
+zip -r $LAMBDA_NAME.zip lambda_function.py
+aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://$LAMBDA_NAME.zip
