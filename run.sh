250a692f2430c5e9fbf0bf21b29f403e7be1eec5
deepak.pawar <deepak.pawar@blazeclan.com>
2023-10-19 10:19:42 +0530

    test

--- run.sh
+++ run.sh
@@ -2,7 +2,15 @@
  
 input_file="variables.json"
  
-jq -r '.employees[] | "\(.name), \(.email)"' "$input_file" |
-while IFS= read -r employee_info; do
-    echo "Employee Info: $employee_info"
+jq -r '.lambda[] | "\(.LAMBDA_NAME), \(.LAMBDA_CFT_NAME), \(.LAMBDA_DESCRIPTION)"' "$input_file" |
+while IFS= read -r lambda_info; do
+ 
+    LAMBDA_NAME=$(echo "$lambda_info" | cut -d ',' -f1)
+    LAMBDA_CFT_NAME=$(echo "$lambda_info" | cut -d ',' -f2)
+    LAMBDA_DESCRIPTION=$(echo "$lambda_info" | cut -d ',' -f3)
+ 
+    echo "LAMBDA_NAME: $LAMBDA_NAME"
+    echo "LAMBDA_CFT_NAME: $LAMBDA_CFT_NAMEl"
+    echo "LAMBDA_DESCRIPTION: $LAMBDA_DESCRIPTION"
+    echo "--------------------------------"
 done
