3bcc4b736cd00e59e7e5fe26618875981c1a8868
deepak.pawar <deepak.pawar@blazeclan.com>
2023-10-19 10:05:00 +0530

    test

--- run.sh
+++ run.sh
@@ -3,8 +3,13 @@
 input_file="variables.json"
  
 jq -r '.lambda_details[] | "\(.LAMBDA_NAME), \(.LAMBDA_DESCRIPTION), \(.LAMBDA_CFT_NAME)"' "$input_file" |
+while IFS= read -r lambda_info; do
+    Lambda Name=$(echo "$lambda_info" | cut -d ',' -f1)
+    Lambda description=$(echo "$lambda_info" | cut -d ',' -f2)
+    Lambda cft name=$(echo "$lambda_info" | cut -d ',' -f3)
 
 
-echo "Lambda Name: $LAMBDA_NAME"
-echo "Lambda description: $LAMBDA_DESCRIPTION"
-echo "Lambda cft name: $age years ($LAMBDA_CFT_NAME)"
+    echo "Lambda Name: $LAMBDA_NAME"
+    echo "Lambda description: $LAMBDA_DESCRIPTION"
+    echo "Lambda cft name: $LAMBDA_CFT_NAME"
+done
