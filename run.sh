d8d03332788b02abae6937dcd46787e69823bfd0
deepak.pawar <deepak.pawar@blazeclan.com>
2023-10-19 10:07:55 +0530

    test

--- run.sh
+++ run.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
  
-input_file="variables.json"
+input_file="example.json"
  
-jq -r '.lambda_details[] | "\(.LAMBDA_NAME), \(.LAMBDA_DESCRIPTION), \(.LAMBDA_CFT_NAME)"' "$input_file" |
-while IFS= read -r lambda_info; do
-    Lambda Name=$(echo "$lambda_info" | cut -d ',' -f1)
-    Lambda description=$(echo "$lambda_info" | cut -d ',' -f2)
-    Lambda cft name=$(echo "$lambda_info" | cut -d ',' -f3)
-
-
-    echo "Lambda Name: $LAMBDA_NAME"
-    echo "Lambda description: $LAMBDA_DESCRIPTION"
-    echo "Lambda cft name: $LAMBDA_CFT_NAME"
+jq -r '.employees[] | "\(.name), \(.email)"' "$input_file" |
+while IFS= read -r employee_info; do
+    echo "Employee Info: $employee_info"
 done
