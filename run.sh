e619a394662881fd90806b84054623f17d58e518
deepak.pawar <deepak.pawar@blazeclan.com>
2023-10-19 10:54:33 +0530

    test

--- run.sh
+++ run.sh
@@ -10,10 +10,15 @@ while IFS= read -r lambda_info; do
     LAMBDA_DESCRIPTION=$(echo "$lambda_info" | cut -d ',' -f3)
  
     echo "LAMBDA_NAME: $LAMBDA_NAME"
+	echo "zip -r $LAMBDA_NAME.zip lambda_function.py"
 	zip -r $LAMBDA_NAME.zip lambda_function.py
+    echo "--------------------------------"
+	echo "aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://$LAMBDA_NAME.zip"
 	aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://$LAMBDA_NAME.zip
+    echo "--------------------------------"
     echo "LAMBDA_CFT_NAME: $LAMBDA_CFT_NAME"
     echo "LAMBDA_DESCRIPTION: $LAMBDA_DESCRIPTION"
-    echo "--------------------------------"
+	echo "aws cloudformation update-stack --stack-name $LAMBDA_CFT_NAME --tags Key=Project,Value=InstaGo Key=Owner,Value=MonishParekh Key=Environment,Value=Production Key=Application,Value=InstaGo Key=Name,Value=$LAMBDA_CFT_NAME --template-body file://$LAMBDA_NAME.yml --parameters file://$input_file --capabilities CAPABILITY_NAMED_IAM"
 	aws cloudformation update-stack --stack-name $LAMBDA_CFT_NAME --tags Key=Project,Value=InstaGo Key=Owner,Value=MonishParekh Key=Environment,Value=Production Key=Application,Value=InstaGo Key=Name,Value=$LAMBDA_CFT_NAME --template-body file://$LAMBDA_NAME.yml --parameters file://$input_file --capabilities CAPABILITY_NAMED_IAM
+    echo "--------------------------------"
 done
