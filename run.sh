a15481f821c23da848dedfaaded222edf80b5856
deepak.pawar <deepak.pawar@blazeclan.com>
2023-10-19 10:23:57 +0530

    test

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ while IFS= read -r lambda_info; do
     LAMBDA_DESCRIPTION=$(echo "$lambda_info" | cut -d ',' -f3)
  
     echo "LAMBDA_NAME: $LAMBDA_NAME"
+	zip -r $LAMBDA_NAME.zip lambda_function.py
+	aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://$LAMBDA_NAME.zip
     echo "LAMBDA_CFT_NAME: $LAMBDA_CFT_NAME"
     echo "LAMBDA_DESCRIPTION: $LAMBDA_DESCRIPTION"
     echo "--------------------------------"
