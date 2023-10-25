347d86e44ba76f3167d889920e479df0a62a2eab
deepak.pawar <deepak.pawar@blazeclan.com>
2023-10-25 18:06:34 +0530

    lambda_3 code update

--- run.sh
+++ run.sh
@@ -7,18 +7,20 @@ while IFS= read -r lambda_info; do
 
  
     LAMBDA_NAME=$(echo "$lambda_info" | cut -d ',' -f1)
-	
-	echo "======================================"  
+
+
 	echo "======================================"  	
 	echo "LAMBDA_NAME: $LAMBDA_NAME"
-	echo "======================================"  
 	echo "======================================"   
+    echo "change lambda.py file name to lambda_function.py"
+	echo "======================================"  
+    mv $LAMBDA_NAME.py lambda_function.py
+	echo "======================================" 	
 	echo "Convert lambda_function file to .zip"
+	echo "======================================" 	
 	zip -r $LAMBDA_NAME.zip lambda_function.py
-    echo "======================================"
-    echo "======================================"
 	echo "Update new lambda code on mention Lambda"
+	echo "======================================"
 	aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://$LAMBDA_NAME.zip
-    echo "======================================"
     echo "======================================"
 done
